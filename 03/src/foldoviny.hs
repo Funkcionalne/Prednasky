2abfe914b28f33d47dd8521062cc68a5642a7f2f
vma2015 <borovansky@gmail.com>
2016-03-01 07:47:33 +0100

    tretia prednaska

---
+++ 03/src/foldoviny.hs
@@ -0,0 +1,27 @@
+module Foldoviny where
+
+powerSet  :: [t] -> [[t]]
+powerSet []     = [[]]
+powerSet (x:xs) = [ x:p | p <- ps ] ++ ps
+                  where ps = powerSet xs
+                  
+powerSet'  :: [t] -> [[t]]
+powerSet' xs = foldr (\x -> \ps -> map (x:) ps ++ ps) [[]] xs
+
+powerSet''  :: [t] -> [[t]]
+powerSet''  = foldr (\x -> \ps -> map (x:) ps ++ ps) [[]]
+
+powerSet'''  :: [t] -> [[t]]
+powerSet'''  = foldr pom [[]] where    
+          pom x ps = map (x:) ps ++ ps
+
+-- naprogramujme take pomocou foldl/r          
+take' :: Int -> [a] -> [a]
+take' n xs  =  (foldr pom (\_ -> []) xs) n where  
+                pom x h = \n -> if n == 0 then []
+                                 else x:(h (n-1))
+
+take'' :: Int -> [a] -> [a]
+take'' n xs  =  (foldr pom (\_ -> []) xs) n where  
+                  pom x h n = if n == 0 then []
+                              else x:(h (n-1))
