b908e212c323d8752778851891749c4704ab61cc
borovan <borovan@microstep-mis.com>
2017-02-19 21:16:43 +0100

    prva prednaska

--- 01/src/Church.hs
+++ 01/src/Church.hs
@@ -23,31 +23,6 @@ decr n = n (\m f x -> f (m incr zero))
            (\x -> x)
            zero
 -------------
-{--
-Tvrdenie:
-decr zero f x = zero
-decr n f x = f^(n-1) x
-Dokaz
-zero (\m f x -> f (m incr zero)) zero id zero = zero id zero = zero
-one (\m f x -> f (m incr zero)) zero id zero = 
-  (\m f x -> f (m incr zero)) zero id zero =
-  (f x -> f (zero incr zero)) id zero =
-  (x -> id (zero incr zero)) zero =
-  (id (zero incr zero)) =
-  (zero incr zero) =
-  zero
-indukcia:
-(incr N) (\m f x -> f (m incr zero)) zero id zero = 
-  (\f x -> f (N f x)) (\m f x -> f (m incr zero)) zero id zero = 
-  (\m f x -> f (m incr zero)) (N (\m g y -> g (m incr zero)) zero) id zero =   ind.hyp.
-  (\m f x -> f (m incr zero)) (g^(n-1) y) id zero =  
-  (\f x -> f ((f^(n-1) y) incr zero))  id zero =  
-  (\x -> ((f^(n-1) x) incr zero))  zero =  
-  ((f^(n-1) x) incr zero) =  ????
-
---}
-
--------------
 fact :: (forall a. (a->a)->a->a) -> (a->a) -> a -> a        
 fact n  =
      ifte (isZero n)
@@ -55,6 +30,7 @@ fact n  =
           (mul n (fact (decr n)))
 
 main = 
+  -- print $ (add (mul two two) one) (+1) 0  
   -- print $ (decr (add (mul two two) one)) (+1) 0  
   -- print $ (fact (add (mul two two) one)) (+1) 0
   print $ (fact (add two (add (mul two two) (mul two two)))) (+1) 0
