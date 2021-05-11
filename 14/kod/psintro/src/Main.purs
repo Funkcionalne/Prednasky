0db8c1a4803513bb3acf5bddddb8cbbe4d540fb1
Matej Fandl <fandl@mladypes.sk>
2021-05-11 11:46:30 +0200

    reorganized functions + code formatted with purty

--- 14/kod/psintro/src/Main.purs
+++ 14/kod/psintro/src/Main.purs
@@ -6,68 +6,45 @@ import Data.Int (toNumber)
 import Data.JSDate (now, getTime)
 import Data.Maybe (fromJust)
 import Data.Unfoldable (replicateA)
-
 import Effect (Effect)
 import Effect.Random (random)
-import Graphics.Canvas ( CanvasElement
-                       , Context2D
-                       , Dimensions
-                       , fillRect
-                       , getCanvasElementById
-                       , getCanvasDimensions
-                       , getContext2D
-                       , setCanvasHeight
-                       , setCanvasWidth
-                       , setFillStyle)
-
+import Graphics.Canvas
+  ( CanvasElement
+  , Context2D
+  , Dimensions
+  , fillRect
+  , getCanvasElementById
+  , getCanvasDimensions
+  , getContext2D
+  , setCanvasHeight
+  , setCanvasWidth
+  , setFillStyle
+  )
 import Math (cos, sin)
-
 import Partial.Unsafe (unsafePartial)
-
 import Web.HTML (Window, window)
-import Web.HTML.Window ( innerHeight
-                       , innerWidth
-                       , requestAnimationFrame)
+import Web.HTML.Window
+  ( RequestAnimationFrameId
+  , innerHeight
+  , innerWidth
+  , requestAnimationFrame
+  )
+
+type Env
+  = { canvas :: CanvasElement
+    , context :: Context2D
+    , window :: Window
+    , positions :: Array Position
+    }
+
+type Position
+  = { x :: Number, y :: Number }
 
 bipolarToPositive :: Number -> Number
 bipolarToPositive x = (1.0 + x) / 2.0
 
-
-type Env = {
-    canvas :: CanvasElement,
-    context :: Context2D,
-    window :: Window,
-    positions :: Array Position
-  }
-
-type Position = { x :: Number, y :: Number }
-
-renderStar :: Env -> Number -> Position -> Effect Unit
-renderStar { context } currentTime position =
-  fillRect context { x: nx
-                   , y: ny
-                   , width: s
-                   , height: s
-                   }
-  where
-    nx = position.x + (sin ((currentTime + position.y) * s / 10.0)) * s * 10.0
-    ny = position.y + (cos ((currentTime + position.x) * s / 10.0)) * s * 5.0
-    s  = 1.0 + (bipolarToPositive (sin (position.x * position.y * 1337.0))) * 5.0
-
 msToS :: Number -> Number
 msToS = (_ / 1000.0)
-
-render :: Env  -> Effect Unit
-render env@{ canvas, window, context, positions } = void $ requestAnimationFrame
-  do
-     render env
-     currentTime <- (getTime >>> msToS) <$> now
-     { width, height } <- getCanvasDimensions canvas
-     setFillStyle context "#577590"
-     fillRect context { x: 0.0, y: 0.0, width, height }
-     setFillStyle context "white"
-     sequence_ $ renderStar env currentTime <$> positions
-  window
 
 randomPosition :: Dimensions -> Effect Position
 randomPosition { width, height } = do
@@ -75,19 +52,50 @@ randomPosition { width, height } = do
   y <- random
   pure { x: x * width, y: y * height }
 
+renderStar :: Env -> Number -> Position -> Effect Unit
+renderStar { context } currentTime position =
+  fillRect context
+    { x: nx
+    , y: ny
+    , width: s
+    , height: s
+    }
+  where
+  nx = position.x + (sin ((currentTime + position.y) * s / 10.0)) * s * 10.0
+
+  ny = position.y + (cos ((currentTime + position.x) * s / 10.0)) * s * 5.0
+
+  s = 1.0 + (bipolarToPositive (sin (position.x * position.y * 1337.0))) * 5.0
+
+render :: Env -> Effect RequestAnimationFrameId
+render env@{ canvas, window, context, positions } =
+  requestAnimationFrame
+    ( do
+        _ <- render env
+        currentTime <- (getTime >>> msToS) <$> now
+        { width, height } <- getCanvasDimensions canvas
+        setFillStyle context "#577590"
+        fillRect context { x: 0.0, y: 0.0, width, height }
+        setFillStyle context "white"
+        sequence_ $ renderStar env currentTime <$> positions
+    )
+    window
 
 main :: Effect Unit
 main = do
   maybeCanvas <- getCanvasElementById "cnvs"
-  let canvas = unsafePartial $ fromJust maybeCanvas
+  let
+    canvas = unsafePartial $ fromJust maybeCanvas
   wnd <- window
   innerWidth wnd >>= setCanvasWidth canvas <<< toNumber
   innerHeight wnd >>= setCanvasHeight canvas <<< toNumber
   ctx <- getContext2D canvas
   cd <- getCanvasDimensions canvas
   positions <- replicateA 100 $ randomPosition cd
-  render { canvas: canvas
-         , context: ctx
-         , window: wnd
-         , positions: positions
-         }
+  void
+    $ render
+        { canvas: canvas
+        , context: ctx
+        , window: wnd
+        , positions: positions
+        }
